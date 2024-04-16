f0a75fb119425ce60e64f3dc6d10465e45b94e7a
johnandrea <46268748+johnandrea@users.noreply.github.com>
2024-04-15 23:01:42 -0300

    Update show-values.sql

--- show-values.sql
+++ show-values.sql
@@ -18,7 +18,7 @@ where NameTable.isPrimary
 and EventTable.OwnerType = 0
 and NameTable.OwnerId = EventTable.OwnerId
 and EventTable.EventType =
-(select FactTypeID from FactTypeTable where Name like'6levels')
+(select FactTypeID from FactTypeTable where Name like '6levels')
 order by NameTable.OwnerId
 ;
 
